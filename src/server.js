7678ee49f8ff865c2ffaee6c4e4eaf16a21ba71d
zxcdet <jallyface@gmail.com>
2025-01-28 12:46:26 +0200

    feat: change connection to db

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import { config } from './common/config.js';
 import { app } from './app.js';
+import { connectDb } from './common/connect-db.js';
 
-app.listen(config.PORT, () =>
-  console.log(`App is running on http://localhost:${config.PORT}`)
-);
+connectDb()
+  .then(() => {
+    console.log('Mysql connect!');
+    app.listen(config.PORT, () =>
+      console.log(`App is running on http://localhost:${config.PORT}`)
+    );
+  })
+  .catch(err => {
+    console.error('mysql error', err);
+  });
