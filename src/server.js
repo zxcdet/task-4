8e6fb81a05fd21dc1ee0b8f1d51951db9c9abdf8
zxcdet <jallyface@gmail.com>
2025-02-07 16:48:03 +0200

    feat: delete mongo from package add admin

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import { config } from './common/config.js';
 import { app } from './app.js';
 import { connectDb } from './common/connect-db.js';
+import { prisma } from './common/prisma-client.js';
 
 async function main() {
   try {
     await connectDb();
+    await prisma.user.create({
+      data: {
+        name: 'admin',
+        login: 'admin',
+        password: 'admin'
+      }
+    });
     console.log('MySQL connected!');
 
     app.listen(config.PORT, () => {
